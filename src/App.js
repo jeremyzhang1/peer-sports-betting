414c95c3e0fc7207301b5d89be34c53246e68009
Charles Ma <70816291+golfcm6@users.noreply.github.com>
2022-01-07 20:02:40 -0500

    first attempt at integrating web3 to website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
+import getWeb3 from './utils/getWeb3';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+
+
+class App extends Component {
+
+  constructor(){
+    super();
+    //define web3 and address in state so we can access and change through react app as needed
+    this.state = {
+      web3 : '',
+      address: '',
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Blockchain Basketball!</p>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    resolve: {
+        fallback: {
+            "stream": false,
+            "crypto": false,
+            "http": false,
+            "https": false,
+            "os": false
+        }
+    }
+}
