810faf6ef052fb9b1352fef7667edeff3acdac4b
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2022-01-08 14:36:20 -0600

    rudimentary demo app working

--- src/App.js
+++ src/App.js
@@ -1,76 +1,99 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Web3 from 'web3';
 import Betting from './contractCode/Betting.json';
-import Migrations from './contractCode/Migrations.json';
-
-
 
 class App extends Component {
 
-  constructor(){
-    super();
-    //define web3 and address in state so we can access and change through react app as needed
-    this.state = {
-      web3 : '',
-      address: '',
-      betting: {}
-    };
-  }
+    constructor() {
+        super();
+        //define web3 and address in state so we can access and change through react app as needed
+        this.state = {
+            web3: '',
+            address: '0x0',
+            betting: {}
+        };
 
-  componentDidMount() {
-    this.loadWeb3();
-    this.loadUserData();
-  }
-
-  async loadWeb3() {
-    if (window.ethereum) {
-      window.web3 = new Web3(window.ethereum)
-      await window.ethereum.enable()
-    }
-    else if (window.web3) {
-      window.web3 = new Web3(window.web3.currentProvider)
-    }
-    else {
-      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
-    }
-  }
-
-  async loadUserData() {
-    var accounts;
-    const web3 = window.web3;
-    accounts = await web3.eth.getAccounts().then();
-    console.log("check account");
-    console.log(typeof accounts);
-    this.setState({ address : accounts });
-    console.log("address state set");
-    console.log(this.state.address);
-    
-    const networkId = await web3.eth.net.getId();
-    console.log(networkId);
-
-    const bettingData = Betting.networks[networkId];
-    if (bettingData) {
-      const betting = web3.eth.Contract(Betting.abi, bettingData.address);
-      this.setState({ betting });
-      // still a bit confused what to send as an arg into methods.bet in the line below
-      // let bet = await betting.methods.bet()
-      
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-  }
+    componentDidMount() {
+        this.loadWeb3();
+        this.loadUserData();
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Blockchain Basketball! {this.state.address} blah</p>
-        </header>
-      </div>
-    )
-  }
+    async loadWeb3() {
+        if (window.ethereum) {
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        }
+        else if (window.web3) {
+            window.web3 = new Web3(window.web3.currentProvider)
+        }
+        else {
+            window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+        }
+    }
+
+    async loadUserData() {
+        // find the metamask account
+        const web3 = window.web3;
+        let accounts = await web3.eth.getAccounts().then();
+        this.setState({ address: accounts[0] });
+
+        // find the network
+        const networkId = await web3.eth.net.getId();
+
+        // find the contract
+        const bettingData = Betting.networks[networkId];
+        if (bettingData) {
+            const betting = new web3.eth.Contract(Betting.abi, bettingData.address);
+            this.setState({ betting });
+
+            // TODO: et the current amount bet on each team
+            // const homeBet = await this.state.betting.methods.AmountHome(1)
+            // const awayBet = await this.state.betting.methods.AmountAway(1)
+        } else {
+            window.alert('Betting contract not deployed to detected network.')
+        }
+
+    }
+
+    // javascript function to call the solidity bet function
+    async bet(team, gameID, amount) {
+        await this.state.betting.methods.bet(team, gameID).send({from: this.state.address, value: window.web3.utils.toWei(amount, "ether")})
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        this.bet(event.target.team.value, event.target.gameid.value, event.target.betamount.value)
+        alert('Bet Submitted')
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <p>Blockchain Basketball</p>
+                <p>Connected wallet address: {this.state.address}</p>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Game ID (1, 2, or 3 for debugging purposes)</label>
+                    <br/>
+                    <input type="number" name="gameid"></input>
+                    <br/>
+                    <label>Amount Bet (in Ether)</label>
+                    <br/>
+                    <input type="number" name="betamount"></input>
+                    <br/>
+                    <label>Which team? (1 for Home, 2 for Away)</label>
+                    <br/>
+                    <input type="number" name="team"></input>
+                    <br/>
+                    <p>Make sure all of the fields are filled out correctly before submitting. <br/> The form currently does not do error checking.</p>
+                    <button type="submit">Submit Bet</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default App;
