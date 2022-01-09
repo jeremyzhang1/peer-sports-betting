88cc9b3cfc81a64ab5a7fc629d836404703bb0f2
Ash Ahmed <ashlanahmed@college.harvard.edu>
2022-01-09 11:53:34 -0500

    get-winners done

--- scripts/get-winner.js
+++ scripts/get-winner.js
@@ -1,27 +1,27 @@
-// Can grab data from API for specific game with id via following code:
-import fetch from "node-fetch";
+import axios from 'axios';
 
-function yestWinners() {
+const Betting = artifacts.require('Betting')
+
+module.exports = async function(callback) {
+    let betting = await Betting.deployed()
     // make an API call
     let yesterday = new Date()
     yesterday.setDate(yesterday.getDate() - 2)
-    let start = yesterday.toISOString().split('T')[0];
-
+    let start = yesterday.toISOString().split('T')[0]
     let url = 'https://www.balldontlie.io/api/v1/games/?seasons[]=2021&start_date=' + start + '&end_date=' + start
-
-    fetch(url).then(response => response.json()).then(response => {
-        const winnerArr = response.data
-        let arrLen = winnerArr.length
-        let output = {}
-        for (let i = 0; i < arrLen; i++) {
-            let game_id = winnerArr[i]['id']
-            let winner = 1
-            if (winnerArr[i]['visitor_team_score'] > winnerArr[i]['home_team_score']) {
-                winner = 2
-            }
-            output[game_id] = winner
-        }    
-        console.log(output)
-    }).catch(console.log)
+    const response = await axios(url);
+    console.log(response.data);
+    // create output dict
+    const winnerArr = response.data
+    let arrLen = winnerArr.length
+    for (let i = 0; i < arrLen; i++) {
+        let game_id = winnerArr[i]['id']
+        let win_side = 1
+        if (winnerArr[i]['visitor_team_score'] > winnerArr[i]['home_team_score']) {
+            win_side = 2
+        }
+        await betting.distributePrizes(win_side, game_id)
+        console.log("Winner of game " + game_id.toString() + " determined to be ", win_side)
+        callback()
+    }
 }
-yestWinners()
