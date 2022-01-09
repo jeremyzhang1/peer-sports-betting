6bcdcc4e0d79f28f5e9f523464deb6a2bda35868
Ash Ahmed <ashlanahmed@college.harvard.edu>
2022-01-08 22:28:00 -0500

    API work done

--- scripts/get-winner.js
+++ scripts/get-winner.js
@@ -1,40 +1,27 @@
 // Can grab data from API for specific game with id via following code:
-
 import fetch from "node-fetch";
 
-// get yesterday date as string
-const yesterday = new Date()
-yesterday.setDate(yesterday.getDate() - 2)
-const start = yesterday.toISOString().split('T')[0];
+function yestWinners() {
+    // make an API call
+    let yesterday = new Date()
+    yesterday.setDate(yesterday.getDate() - 2)
+    let start = yesterday.toISOString().split('T')[0];
 
-const url = 'https://www.balldontlie.io/api/v1/games/?seasons[]=2021&start_date=' + start + '&end_date=' + start
+    let url = 'https://www.balldontlie.io/api/v1/games/?seasons[]=2021&start_date=' + start + '&end_date=' + start
 
-let jsondata;    
-fetch(url)
-  .then(res => res.json())
-  .then(data => jsondata = data)
-  .then(() => console.log(jsondata),
-              jsondata.data)
-
-
-// const calculateReturn = (product) => {
-//     // make an API call here for historical data
-//     let formerPrice = 0;
-//     let currentPrice = 0;
-//     let url = 'https://api.coingecko.com/api/v3/coins/' + nameToId.get(product) + '/history?date=' + formatDate(date) + '&localization=false'
-//     fetch(url).then(response => response.json()).then(response => {
-//         console.log(response)
-//         formerPrice = response.market_data.current_price.usd
-//     }).then(() => {
-//         //make an api call for current data
-//         url = 'https://api.coingecko.com/api/v3/coins/' + nameToId.get(product) + '/history?date=' + formatDate(today) + '&localization=false'
-//         fetch(url).then(response => response.json()).then(response => {
-//             currentPrice = response.market_data.current_price.usd
-//         }).then(() => {
-//             //calculate the proportional return
-//             console.log(currentPrice + " current")
-//             console.log(formerPrice + " former")
-//             setProdRet((currentPrice - formerPrice) / formerPrice * 100)
-//         }).catch(console.log)
-//     }).catch(console.log)
-// }
+    fetch(url).then(response => response.json()).then(response => {
+        const winnerArr = response.data
+        let arrLen = winnerArr.length
+        let output = {}
+        for (let i = 0; i < arrLen; i++) {
+            let game_id = winnerArr[i]['id']
+            let winner = 1
+            if (winnerArr[i]['visitor_team_score'] > winnerArr[i]['home_team_score']) {
+                winner = 2
+            }
+            output[game_id] = winner
+        }    
+        console.log(output)
+    }).catch(console.log)
+}
+yestWinners()
