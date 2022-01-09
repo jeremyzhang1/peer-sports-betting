f88be69ab2768c918bfbff5cf80a5bd3bc17aef4
Charles Ma <70816291+golfcm6@users.noreply.github.com>
2022-01-09 13:10:54 -0500

    fixed es package errors

---
+++ scripts/testing.js
@@ -0,0 +1,9 @@
+const Betting = artifacts.require('Betting')
+
+module.exports = async function(callback) {
+  let betting = await Betting.deployed()
+  let winner = 1
+  await betting.distributePrizes(winner, 1)
+  console.log("Winner determined to be ", winner)
+  callback()
+}
