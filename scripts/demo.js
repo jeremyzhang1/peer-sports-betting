8d604b3e2609d9c4111fbdea65a1748a1a823614
jeremyzhang1 <zhang.jeremy.2001@gmail.com>
2022-01-12 20:25:30 -0600

    update demo

--- scripts/demo.js
+++ scripts/demo.js
@@ -3,7 +3,7 @@ const Betting = artifacts.require('Betting')
 module.exports = async function(callback) {
     let betting = await Betting.deployed()
     let winner = 1
-    let gameid = 474001 // change this for the demo
+    let gameid = 474005 // change this for the demo
     await betting.distributePrizes(winner, gameid)
     callback()
 }
